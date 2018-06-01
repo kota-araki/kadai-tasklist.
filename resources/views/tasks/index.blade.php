9740b61bcb630f000fc22e3cb6477a49564a2134
kota.araki <ec2-user@ip-172-31-5-76.us-east-2.compute.internal>
2018-06-01 04:00:29 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,7 +6,7 @@
         <ul>
             @foreach ($tasks as $task)
                 <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} :  {{ $task->status }} > {{ $task->content }}
-                
+                </li>
             @endforeach
         </ul>
     @endif
