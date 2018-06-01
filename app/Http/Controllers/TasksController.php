ab83fcb68364928b0da8b9139d0b7d3180f3edd3
kota.araki <ec2-user@ip-172-31-5-76.us-east-2.compute.internal>
2018-06-01 00:32:53 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -97,8 +97,8 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
       $this->validate($request, [     
-          'status' => 'required|max:191', 
-          'content' => 'required|max:191',   
+          'status' => 'required|max:10', 
+          'content' => 'required|max:10',   
           ]); 
  
 
